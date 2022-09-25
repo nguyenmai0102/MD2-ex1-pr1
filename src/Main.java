2626a4f0439e940b1c7ca3814950bd4a02aef329
unknown <nguyenmai0102.meo@gmail.com>
2022-09-25 20:35:31 +0700

    noi dung

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Date date   = new Date();
+        System.out.println("now is " + date);
+
+    }
+}
